093f8ddb615cc96e816d2b83c46c85e0485dd0fa
cwj <v_wwjjchen@tencentmusic.com>
2024-07-18 22:40:55 +0800

    升级

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="FlawlessCICD",
-    version="0.1.0",
+    version="0.2.0",
     author="wen",
     author_email="799771344@qq.com",
     description="本地可使用的的CI/CD持续集成（Continuous Integration）和持续交付/部署（Continuous Delivery/Deployment），以最方便快捷的方式将本地项目部署到服务器上",
